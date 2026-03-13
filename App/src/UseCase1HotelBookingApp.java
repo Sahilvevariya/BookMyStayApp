8395e963cc86a736ca86f3e560e14c4bf0e5b49c
Sahilvevariya <sahil.vevariya10948@gmail.com>
2026-03-13 10:34:47 +0530

    Added feature/UC1-WelcomePage

---
+++ App/src/UseCase1HotelBookingApp.java
@@ -0,0 +1,35 @@
+/**
+ * =========================================================
+ * MAIN CLASS – UseCase1HotelBookingApp
+ * =========================================================
+ *
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * Description:
+ * This class represents the entry point of the
+ * Hotel Booking Management System.
+ *
+ * @author Developer
+ * @version 1.0
+ */
+
+public class UseCase1HotelBookingApp {
+
+    /**
+     * Application entry point.
+     * This is the first method executed by the JVM.
+     *
+     * @param args command-line arguments
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println("======================================");
+        System.out.println(" Welcome to the Hotel Booking System ");
+        System.out.println("======================================");
+        System.out.println("Application Name : Book My Stay App");
+        System.out.println("Version          : 1.0");
+        System.out.println("System initialized successfully.");
+
+    }
+}
